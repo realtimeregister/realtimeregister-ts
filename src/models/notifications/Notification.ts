eeb04d4ad489af6466828d6ee44ad96eae5e3f00
pieterjan.eilers <pieterjan.eilers@realtimeregister.com>
2025-09-03 14:38:43 +0200

    fix(Notification): Add 'processType', 'processIdentifier', 'deliveryDate' to Notification.ts

--- src/models/notifications/Notification.ts
+++ src/models/notifications/Notification.ts
@@ -5,16 +5,16 @@ export interface INotification {
   fireDate: Date
   readDate?: Date
   acknowledgeDate?: Date
+  deliveryDate?: Date
   message: string
   reason?: string
   process?: number
   payload?: object
-  customer: string,
+  customer: string
   isAsync: boolean
+  processType?: string
+  processIdentifier?: string
 }
-
-export type NotificationField = keyof INotification
-export type NotificationFilterField = Exclude<NotificationField, 'process' | 'payload' | 'customer' | 'isAsync'>
 
 export class Notification implements INotification {
   id: number
@@ -23,12 +23,15 @@ export class Notification implements INotification {
   fireDate: Date
   readDate?: Date
   acknowledgeDate?: Date
+  deliveryDate?: Date
   message: string
   reason?: string
   process?: number
   payload?: object
   customer: string
   isAsync: boolean
+  processType?: string
+  processIdentifier?: string
 
   constructor (notification: INotification) {
     this.id = notification.id
@@ -37,12 +40,15 @@ export class Notification implements INotification {
     this.fireDate = notification.fireDate
     this.readDate = notification.readDate
     this.acknowledgeDate = notification.acknowledgeDate
+    this.deliveryDate = notification.deliveryDate
     this.message = notification.message
     this.reason = notification.reason
     this.process = notification.process
     this.payload = notification.payload
     this.customer = notification.customer
     this.isAsync = notification.isAsync
+    this.processType = notification.processType
+    this.processIdentifier = notification.processIdentifier
   }
 }
 
