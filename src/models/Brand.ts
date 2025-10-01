6671a8ab64bcc6f43c085b4538c12d8bd057fb34
Hylke Witjens <hylke.witjens@realtimeregister.com>
2025-10-01 12:57:00 +0200

    feat(Brand): Invalid SPF field @ brands

--- src/models/Brand.ts
+++ src/models/Brand.ts
@@ -35,6 +35,7 @@ export interface IBrand {
   abuseContact?: string
   createdDate: Date
   updatedDate?: Date
+  invalidSpfSince?: Date
 }
 
 export type BrandField = keyof IBrand
@@ -58,6 +59,7 @@ export default class Brand implements IBrand {
   abuseContact?: string
   createdDate: Date
   updatedDate?: Date
+  invalidSpfSince?: Date
 
   constructor (brand: IBrand) {
     this.handle = brand.handle
@@ -78,5 +80,6 @@ export default class Brand implements IBrand {
     this.abuseContact = brand.abuseContact
     this.createdDate = brand.createdDate ? new Date(brand.createdDate) : brand.createdDate
     this.updatedDate = brand.updatedDate ? new Date(brand.updatedDate) : brand.updatedDate
+    this.invalidSpfSince = brand.invalidSpfSince ? new Date(brand.invalidSpfSince) : brand.invalidSpfSince
   }
 }
