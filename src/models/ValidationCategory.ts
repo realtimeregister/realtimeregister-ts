a95aeec1693d85a3ad203e7c1e3f7bb036a354aa
pieterjan.eilers <pieterjan.eilers@realtimeregister.com>
2025-10-15 14:04:04 +0200

    feat(Validation): Added validation categories endpoints

--- src/models/ValidationCategory.ts
+++ src/models/ValidationCategory.ts
@@ -1,14 +1,34 @@
-export interface IValidationCategoryTerms {
-  id: number
+export interface ValidationCategoryTerms {
   version: number
   validUntil: Date
   terms: string
 }
 
 export interface IValidationCategory {
-  id: number
+  version: number
   name: string
   description: string
   fields: string[]
-  terms: IValidationCategoryTerms[]
+  terms: ValidationCategoryTerms[]
 }
+
+export type ValidationCategoryField = Exclude<ValidationCategory, 'version' | 'description' | 'fields' | 'terms'>
+export type ValidationCategoryFilterField = ValidationCategoryField
+
+
+export default class ValidationCategory {
+  version: number
+  name: string
+  description: string
+  fields: string[]
+  terms: ValidationCategoryTerms[]
+
+  constructor(category: IValidationCategory) {
+    this.version = category.version
+    this.name = category.name
+    this.description = category.description
+    this.fields = category.fields
+    this.terms = category.terms
+  }
+}
+
