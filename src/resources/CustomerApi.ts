270875ca22ab121c6e9a394cac9ce05d50300c25
kevinkosterr <koster.k2001@gmail.com>
2025-11-18 09:51:28 +0100

    fix(CustomerApi): priceList response
    Fixes #50

--- src/resources/CustomerApi.ts
+++ src/resources/CustomerApi.ts
@@ -1,6 +1,6 @@
 import Base from '@/resources/Base.ts'
 import Credit from '@/models/Credit.ts'
-import Price from '@/models/Price.ts'
+import PriceList, { type IPriceList } from '@/models/PriceList.ts'
 
 export default class CustomerApi extends Base {
 
@@ -9,8 +9,8 @@ export default class CustomerApi extends Base {
       .then(response => new Credit(response.data))
   }
 
-  async priceList (currency?: 'USD'|'EUR'): Promise<Price> {
-    return this.axios.get('/customers/' + this.customer + '/pricelist', { params: currency ? { currency } : undefined })
-      .then(response => new Price(response.data))
+  async priceList (currency?: 'USD'|'EUR'): Promise<PriceList> {
+    return this.axios.get<IPriceList>('/customers/' + this.customer + '/pricelist', { params: currency ? { currency } : undefined })
+      .then(response => new PriceList(response.data))
   }
 }
