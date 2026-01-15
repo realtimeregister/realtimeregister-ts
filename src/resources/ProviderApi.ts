19a02f690daab015288004e13dffa541291630f4
kevinkosterr <koster.k2001@gmail.com>
2026-01-15 10:11:51 +0100

    fix(ProviderApi): missing get downtime window endpoint
    Fixes #78

--- src/resources/ProviderApi.ts
+++ src/resources/ProviderApi.ts
@@ -1,7 +1,13 @@
 import Base from '@/resources/Base.ts'
 import { ProviderDownTimeWindowListParams, ProviderListParams } from '@/models/ListParams.ts'
 import Page from '@/models/Page.ts'
-import { ProviderDowntimeWindow, Provider, IProvider, IProviderDowntimeWindow } from '@/models/Provider.ts'
+import {
+  ProviderDowntimeWindow,
+  Provider,
+  IProvider,
+  IProviderDowntimeWindow,
+  ProviderDowntimeWindowField
+} from '@/models/Provider.ts'
 import { CancelToken } from 'axios'
 
 export default class ProviderApi extends Base {
@@ -34,6 +40,19 @@ export default class ProviderApi extends Base {
         const entities: ProviderDowntimeWindow[] = (response.data.entities || []).map((data: IProviderDowntimeWindow) => new ProviderDowntimeWindow(data))
         return new Page<ProviderDowntimeWindow>(entities, response.data.pagination.limit, response.data.pagination.offset, response.data.pagination.total)
       })
+  }
+
+  /**
+   * Get a provider downtime window
+   * @link https://dm.realtimeregister.com/docs/api/providers/downtime/get
+   * @param downtimeId - ID of the downtime window to get.
+   * @param fields - Fields to include in the response, see ProviderDowntimeWindowField
+   * @see ProviderDowntimeWindowField
+   * @see listDowntimeWindows
+   */
+  async getDowntimeWindow (downtimeId: number, fields?: ProviderDowntimeWindowField[]): Promise<ProviderDowntimeWindow> {
+    return this.axios.get<IProviderDowntimeWindow>('/providers/downtime/' + downtimeId, { params: { fields } })
+      .then(response => new ProviderDowntimeWindow(response.data))
   }
 
 }
