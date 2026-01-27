a7040bd233156e1be56671428c4a81e92c920fbc
kevinkosterr <koster.k2001@gmail.com>
2026-01-27 13:19:47 +0100

    fix(BillingApi): add missing `listExchangeRates()` endpoint

--- src/resources/BillingApi.ts
+++ src/resources/BillingApi.ts
@@ -42,4 +42,14 @@ export default class BillingApi extends Base {
       .then(response => new ExchangeRate(response.data))
   }
 
+  /**
+   * List available exchange rates.
+   * @link https://dm.realtimeregister.com/docs/api/exchangerates/list
+   */
+  async listExchangeRates(): Promise<ExchangeRate[]> {
+    return this.axios.get<IExchangeRate[]>('/exchangerates/').then((response) => {
+      return response.data.map((data: IExchangeRate) => new ExchangeRate(data))
+    })
+  }
+
 }
